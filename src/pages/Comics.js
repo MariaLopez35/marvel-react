25964c4cfc3a7676880d522ce9fd4e8257c4c8c4
Maria Lopez <marialopezjava@gmail.com>
2025-04-07 15:13:43 +0200

    feat: adding images and routes

---
+++ src/pages/Comics.js
@@ -0,0 +1,10 @@
+export default function Comics(){
+
+    return(
+        <div>
+            <p>
+               Estamos dentro de comics 
+            </p>
+        </div>
+    );
+}
---
+++ src/pages/Series.js
@@ -0,0 +1,10 @@
+export default function Series(){
+
+    return(
+        <div>
+            <p>
+               Estamos desde series 
+            </p>
+        </div>
+    );
+}
